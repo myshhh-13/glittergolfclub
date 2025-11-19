edab6204a6002d66db3fb748e60ca35305ed680c
htmlmysh <mysh.lynnn@gmail.com>
2025-11-19 14:47:58 +0300

    Added basic functionality

---
+++ app/config.py
@@ -0,0 +1,3 @@
+class Config:
+    with open('VERSION', 'r', encoding='utf-8') as file:
+        VERSION = file.read()
---
+++ app/routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, current_app, render_template
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html', title='Glitter Club', version=current_app.config['VERSION'])
