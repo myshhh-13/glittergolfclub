ee6439d7d7d208a7945d336d49f94962a41c28e8
htmlmysh <mysh.lynnn@gmail.com>
2025-11-28 06:20:02 +0300

    Maybe now

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,16 @@ from flask import Flask
 from .config import Config
 from .routes import bp as main_bp
 
+
 def create_app():
-    app = Flask(__name__)
+    conf = Config()
+
+    if conf.ENVIRONMENT == 'dev':
+        static_url_path = '/dev/static'
+    else:
+        static_url_path = '/static'
+
+    app = Flask(__name__, static_url_path=static_url_path)
     app.config.from_object(Config)
     app.register_blueprint(main_bp)
 
