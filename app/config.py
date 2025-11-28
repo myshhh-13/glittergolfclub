e72305571f09b80f8ea483672734bb34755d59c3
htmlmysh <mysh.lynnn@gmail.com>
2025-11-28 05:41:04 +0300

    Add dotenv

--- app/config.py
+++ app/config.py
@@ -1,3 +1,10 @@
+import os
+from dotenv import load_dotenv
+
 class Config:
+    load_dotenv('app/.env')
+
+    ENVIRONMENT = os.getenv('ENVIRONMENT')
+
     with open('VERSION', 'r', encoding='utf-8') as file:
         VERSION = file.read()
--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('index.html', title='Glitter Club', version=current_app.config['VERSION'])
+    return render_template('index.html', title='Glitter Club', version=current_app.config['VERSION'], env=current_app.config['ENVIRONMENT'])
