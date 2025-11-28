3c695b1be095ecad96688c69c2002db4163039d7
htmlmysh <mysh.lynnn@gmail.com>
2025-11-28 08:49:33 +0300

    Test colors and add font

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,18 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html', title='Glitter Club', version=current_app.config['VERSION'], env=current_app.config['ENVIRONMENT'])
+
+
+@bp.route('/contactus')
+def contactus():
+    return render_template('contactus.html', title='Glitter Club', version=current_app.config['VERSION'], env=current_app.config['ENVIRONMENT'])
+
+
+@bp.route('/news')
+def news():
+    return render_template('news.html', title='Glitter Club', version=current_app.config['VERSION'], env=current_app.config['ENVIRONMENT'])
+
+
+@bp.route('/about')
+def about():
+    return render_template('about.html', title='Glitter Club', version=current_app.config['VERSION'], env=current_app.config['ENVIRONMENT'])
