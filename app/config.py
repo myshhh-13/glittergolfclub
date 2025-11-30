c18dc3cccda4ab6b4b3778f67b8f2707a10a2ee7
htmlmysh <mysh.lynnn@gmail.com>
2025-11-30 20:21:40 +0300

    Fix timedelta bug

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 
 class Config:
@@ -8,7 +9,7 @@ class Config:
 
     IGNORE_CLIENTS_AFTER = os.getenv('IGNORE_CLIENTS_AFTER')
     BLACKLIST_CLIENTS_AFTER = os.getenv('BLACKLIST_CLIENTS_AFTER')
-    FORGIVE_TIME = os.getenv('FORGIVE_TIME')
+    FORGIVE_TIME = timedelta(minutes=int(os.getenv('FORGIVE_TIME')))
 
     with open('VERSION', 'r', encoding='utf-8') as file:
         VERSION = file.read()
