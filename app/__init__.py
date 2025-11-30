55871f3bc7a5636e894a423a20807e5c78b7ae40
htmlmysh <mysh.lynnn@gmail.com>
2025-11-30 18:34:32 +0300

    Add fix to not showing real ip

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from .config import Config
 from .routes import bp as main_bp
@@ -6,6 +7,7 @@ from .routes import bp as main_bp
 
 def create_app():
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
     app.config.from_object(Config)
     app.register_blueprint(main_bp)
 
